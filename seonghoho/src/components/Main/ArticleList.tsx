ae726cb282fb607572c671190e8b619e9b36d7cf
choiseongho <chltjdgh3@naver.com>
2024-06-13 11:26:00 +0900

    Feat: 무한스크롤 적용

--- seonghoho/src/components/Main/ArticleList.tsx
+++ seonghoho/src/components/Main/ArticleList.tsx
@@ -1,41 +1,63 @@
-import React, { FunctionComponent } from 'react';
-import styled from '@emotion/styled';
-import ArticleDetail from 'components/Main/ArticleDetail';
-import { ArticleListItemType } from 'types/ArticleDetailType';
+import React, { FunctionComponent, useMemo } from 'react'
+import styled from '@emotion/styled'
+import ArticleDetail from 'components/Main/ArticleDetail'
+import { ArticleListItemType } from 'types/ArticleDetailType'
+import useInfiniteScroll, {
+  useInfiniteScrollType,
+} from 'hooks/useInfiniteScroll'
 
 type ArticleListProps = {
+  selectedCategory: string
   articles: ArticleListItemType[]
 }
 
 const ArticleListWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 20px;
-    width: 768px;
-    margin: 0 auto;
-    padding: 50px 0 100px;
-  
-    @media (max-width: 768px) {
-        grid-template-columns: 1fr;
-        width: 100%;
-        padding: 50px 20px;
-    }
-`;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
 
-const ArticleList: FunctionComponent<ArticleListProps> = function({ articles }) {
-  return <ArticleListWrapper>
-    {articles.map(
-      ({
-         node: { id, frontmatter },
-       }: ArticleListItemType) => (
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    width: 100%;
+    padding: 50px 20px;
+  }
+`
+
+const ArticleList: FunctionComponent<ArticleListProps> = function ({
+  selectedCategory,
+  articles,
+}) {
+  const articleListData = useMemo(
+    () =>
+      articles.filter(
+        ({
+          node: {
+            frontmatter: { categories },
+          },
+        }: ArticleListItemType) =>
+          selectedCategory !== 'All'
+            ? categories.includes(selectedCategory)
+            : true,
+      ),
+    [selectedCategory],
+  )
+
+  const { containerRef, articleList }: useInfiniteScrollType =
+    useInfiniteScroll(selectedCategory, articleListData)
+  return (
+    <ArticleListWrapper ref={containerRef}>
+      {articleList.map(({ node: { id, frontmatter } }: ArticleListItemType) => (
         <ArticleDetail
           {...frontmatter}
           link="https://www.google.co.kr/"
           key={id}
         />
-      ),
-    )}
-  </ArticleListWrapper>;
-};
+      ))}
+    </ArticleListWrapper>
+  )
+}
 
-export default ArticleList;
+export default ArticleList
