ff9c2dc294c0c9a71a1cad119cf073cfbbca78a6
choiseongho <chltjdgh3@naver.com>
2024-06-14 14:01:33 +0900

    Style: 게시글 반응형 적용

--- src/components/Article/ArticleHead.tsx
+++ src/components/Article/ArticleHead.tsx
@@ -1,49 +1,57 @@
-import React, {FunctionComponent} from 'react'
+import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
-import {GatsbyImage, IGatsbyImageData} from 'gatsby-plugin-image'
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
 import ArticleHeadInfo from './ArticleHeadInfo'
 
 type GatsbyImgProps = {
-    image: IGatsbyImageData
-    alt: string
-    className?: string
+  image: IGatsbyImageData
+  alt: string
+  className?: string
 }
 
 type ArticleHeadProps = {
-    title: string
-    date: string
-    categories: string[]
-    thumbnail: IGatsbyImageData
+  title: string
+  date: string
+  categories: string[]
+  thumbnail: IGatsbyImageData
 }
 
 const ArticleHeadWrapper = styled.div`
-    position: relative;
-    width: 100%;
-    height: 400px;
+  position: relative;
+  width: 100%;
+  height: 400px;
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const BackgroundImage = styled((props: GatsbyImgProps) => (
-    <GatsbyImage {...props} style={{position: 'absolute'}}/>
+  <GatsbyImage {...props} style={{ position: 'absolute' }} />
 ))`
-    z-index: -1;
-    width: 100%;
-    height: 400px;
-    object-fit: cover;
-    filter: brightness(0.25);
+  z-index: -1;
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+  filter: brightness(0.25);
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const ArticleHead: FunctionComponent<ArticleHeadProps> = function ({
-                                                                       title,
-                                                                       date,
-                                                                       categories,
-                                                                       thumbnail,
-                                                                   }) {
-    return (
-        <ArticleHeadWrapper>
-            <BackgroundImage image={thumbnail} alt="thumbnail"/>
-            <ArticleHeadInfo title={title} date={date} categories={categories}/>
-        </ArticleHeadWrapper>
-    )
+  title,
+  date,
+  categories,
+  thumbnail,
+}) {
+  return (
+    <ArticleHeadWrapper>
+      <BackgroundImage image={thumbnail} alt="thumbnail" />
+      <ArticleHeadInfo title={title} date={date} categories={categories} />
+    </ArticleHeadWrapper>
+  )
 }
 
 export default ArticleHead
