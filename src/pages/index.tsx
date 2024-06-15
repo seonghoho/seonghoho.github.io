ff9c82946dacd5ec0a30513bb0570f31db6f7339
choiseongho <chltjdgh3@naver.com>
2024-06-15 14:29:42 +0900

    Feat: 소개페이지, 블로그 분리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,9 @@
-import React, { FC, useMemo } from 'react'
-import CategoryList, {
-  CategoryListProps,
-} from '../components/Main/CategoryList'
-import Introduction from '../components/Main/Introduction'
-import ArticleList from '../components/Main/ArticleList'
-import { graphql } from 'gatsby'
-import { ArticleListItemType } from '../types/ArticleDetailType'
-import { IGatsbyImageData } from 'gatsby-plugin-image'
-import queryString, { ParsedQuery } from 'query-string'
-import Template from '../components/Common/Template'
+import React, { FunctionComponent } from 'react'
 import NavBar from 'components/Common/NavBar'
+import { graphql } from 'gatsby'
+import Template from 'components/Common/Template'
 
 type IndexPageProps = {
-  location: {
-    search: string
-  }
   data: {
     site: {
       siteMetadata: {
@@ -23,62 +12,20 @@ type IndexPageProps = {
         siteUrl: string
       }
     }
-    allMarkdownRemark: {
-      edges: ArticleListItemType[]
-    }
     file: {
-      childImageSharp: {
-        gatsbyImageData: IGatsbyImageData
-      }
       publicURL: string
     }
   }
 }
 
-const IndexPage: FC<IndexPageProps> = function ({
-  location: { search },
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
   data: {
     site: {
       siteMetadata: { title, description, siteUrl },
     },
-    allMarkdownRemark: { edges },
-    file: {
-      childImageSharp: { gatsbyImageData },
-      publicURL,
-    },
+    file: { publicURL },
   },
 }) {
-  const parsed: ParsedQuery<string> = queryString.parse(search)
-  const selectedCategory: string =
-    typeof parsed.category !== 'string' || !parsed.category
-      ? 'All'
-      : parsed.category
-
-  const categoryList = useMemo(
-    () =>
-      edges.reduce(
-        (
-          list: CategoryListProps['categoryList'],
-          {
-            node: {
-              frontmatter: { categories },
-            },
-          }: ArticleListItemType,
-        ) => {
-          categories.forEach(category => {
-            if (list[category] === undefined) list[category] = 1
-            else list[category]++
-          })
-
-          list['All']++
-
-          return list
-        },
-        { All: 0 },
-      ),
-    [],
-  )
-
   return (
     <Template
       title={title}
@@ -87,19 +34,13 @@ const IndexPage: FC<IndexPageProps> = function ({
       image={publicURL}
     >
       <NavBar />
-      <Introduction profileImage={gatsbyImageData} />
-      <CategoryList
-        selectedCategory={selectedCategory}
-        categoryList={categoryList}
-      />
-      <ArticleList selectedCategory={selectedCategory} articles={edges} />
     </Template>
   )
 }
 
 export default IndexPage
 
-export const getArticleList = graphql`
+export const getInformation = graphql`
   query getArticleList {
     site {
       siteMetadata {
@@ -108,36 +49,7 @@ export const getArticleList = graphql`
         siteUrl
       }
     }
-    allMarkdownRemark(
-      sort: {
-        fields: [frontmatter___date, frontmatter___title]
-        order: [DESC, ASC]
-      }
-    ) {
-      edges {
-        node {
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            summary
-            date(formatString: "YYYY.MM.DD.")
-            categories
-            thumbnail {
-              childImageSharp {
-                gatsbyImageData(width: 768, height: 400)
-              }
-            }
-          }
-        }
-      }
-    }
     file(name: { eq: "profile-image" }) {
-      childImageSharp {
-        gatsbyImageData(width: 120, height: 120)
-      }
       publicURL
     }
   }
