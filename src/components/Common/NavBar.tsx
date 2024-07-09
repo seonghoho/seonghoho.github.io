4d23163c54b3fbc94be8ef3b0dddb1ec9eb74e00
choiseongho <chltjdgh3@naver.com>
2024-07-09 19:57:59 +0900

    feat: 스크롤 위치 및 메인 인지에 따른 navbar 배경 조건부 렌더링

--- src/components/Common/NavBar.tsx
+++ src/components/Common/NavBar.tsx
@@ -1,20 +1,25 @@
-import { FunctionComponent } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import { navigate } from 'gatsby'
 import SeonghoLogo from '../../images/SlimLogo.svg'
+import { useLocation } from '@reach/router'
 
-// nav 태그 div
-const NavDiv = styled.nav`
+const NavDiv = styled.nav<{
+  isScrolled: boolean
+  isMain: boolean
+}>`
   width: 100vw;
   height: 65px;
   position: fixed;
-  background-color: #fff;
-  border-bottom: #d9d9d9 solid 1px;
-  // 가로 정렬
+  background-color: ${props => (props.isScrolled ? '#fff' : 'transparent')};
+  border-bottom: ${props => (props.isMain ? '0' : 'solid 1px #d9d9d9')};
   display: flex;
   top: 0;
   flex-direction: row;
   z-index: 100;
+  transition:
+    background-color 0.3s,
+    border-bottom 0.3s;
 `
 
 const Container = styled.div`
@@ -23,6 +28,7 @@ const Container = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 1024px;
+  margin-top: -65px;
   margin: 0 auto;
 
   @media (max-width: 1050px) {
@@ -30,11 +36,13 @@ const Container = styled.div`
     margin: 0 20px;
   }
 `
+
 const NavList = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: end;
 `
+
 const LogoDiv = styled.div`
   margin-bottom: -10px;
   &:hover {
@@ -54,9 +62,36 @@ const NavItem = styled.div`
     cursor: pointer;
   }
 `
-const NavBar: FunctionComponent = () => {
+
+const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY
+    if (scrollTop > 0) {
+      setIsScrolled(true)
+    } else {
+      setIsScrolled(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  // 메인페이지 인지 확인하기 위한 로직
+  const location = useLocation()
+  const [main, useMain] = useState<boolean>(false)
+
+  useEffect(() => {
+    location.pathname === '/' ? useMain(true) : useMain(false)
+  }, [])
+
   return (
-    <NavDiv>
+    <NavDiv isScrolled={isScrolled} isMain={main}>
       <Container>
         <LogoDiv onClick={() => navigate('/')}>
           <Logo src={SeonghoLogo} />
