b84b57c774d8000b409dfeb270f66a1bab9b8009
choiseongho <chltjdgh3@naver.com>
2024-07-09 12:06:57 +0900

    style: 코드 블럭 style 수정

--- src/components/Article/ArticleContent.tsx
+++ src/components/Article/ArticleContent.tsx
@@ -95,16 +95,29 @@ const MarkdownRenderer = styled.div`
     margin: 30px 0;
     padding: 15px;
     font-size: 15px;
+    border-radius: 5px; /* 코드 블럭 모서리 둥글게 */
+    overflow-x: auto; /* 코드 블럭 가로 스크롤 */
+
+    ::-webkit-scrollbar {
+      height: 8px; /* 스크롤바 높이 */
+    }
 
     ::-webkit-scrollbar-thumb {
-      background: rgba(255, 255, 255, 0.5);
-      border-radius: 3px;
+      background: rgba(0, 0, 0, 0.2); /* 스크롤바 색상 */
+      border-radius: 4px; /* 스크롤바 모서리 둥글게 */
     }
   }
 
   code[class*='language-'],
+
+  // 백틱으로  설정한 코드 블럭
   pre[class*='language-'] {
     tab-size: 2;
+    font-family: 'Noto Sans KR', 'Fira Code', 'Courier New', Courier, monospace;
+    font-size: 0.8rem;
+    padding: 0.2rem 0.4rem;
+    margin: 0 0.2rem;
+    color: #fff;
   }
 
   // Markdown Responsive Design
