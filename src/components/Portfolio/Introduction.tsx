5d298e1089930d6a7067414724301c485003bda9
choiseongho <chltjdgh3@naver.com>
2024-07-12 23:42:14 +0900

    feat: 메인 반응형 추가

--- src/components/Portfolio/Introduction.tsx
+++ src/components/Portfolio/Introduction.tsx
@@ -2,6 +2,9 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import { IGatsbyImageData } from 'gatsby-plugin-image'
 import ProfileImage from '../Main/ProfileImage'
+import Phone from '../../images/Phone.svg'
+import Message from '../../images/Message.svg'
+import Github from '../../images/Github.svg'
 
 type IntroductionProps = {
   profileImage: IGatsbyImageData
@@ -21,6 +24,7 @@ const Wrapper = styled.div`
   width: 1024px;
   height: calc(100vh);
   margin: 0 auto;
+  padding-top: 65px;
 
   @media (max-width: 1050px) {
     width: calc(100% - 40px);
@@ -28,6 +32,7 @@ const Wrapper = styled.div`
 
   @media (max-width: 800px) {
     margin: 20px auto;
+    padding-top: 0px;
   }
 `
 
@@ -44,7 +49,6 @@ const RowDiv = styled.div`
   }
 `
 
-// 연락처 전체 div
 const Contact = styled.div`
   width: calc(50% - 40px);
   display: flex;
@@ -57,8 +61,9 @@ const Contact = styled.div`
   }
 `
 
-// 연락처 개별 item div
 const ContactItem = styled.div`
+  display: flex;
+  flex-direction: row;
   margin: 10px;
   font-size: 20px;
 
@@ -71,14 +76,13 @@ const ContactItem = styled.div`
   }
 `
 
-// 제목 전체 div
 const TitleDiv = styled.div`
   width: 100%;
   @media (max-width: 800px) {
     margin: 20px 0;
   }
 `
-// 큰 sub 제목
+
 const SubTitle = styled.div`
   font-size: 40px;
   font-weight: 500;
@@ -87,15 +91,11 @@ const SubTitle = styled.div`
     font-size: 30px;
   }
 
-  /* @media (max-width: 800px) {
-    font-size: 20px;
-  } */
-
   @media (max-width: 500px) {
     font-size: 22px;
   }
 `
-// 큰 제목
+
 const Title = styled.div`
   margin-top: 5px;
   font-size: 60px;
@@ -104,18 +104,46 @@ const Title = styled.div`
   @media (max-width: 1050px) {
     font-size: 40px;
   }
-  /* @media (max-width: 800px) {
-    font-size: 25px;
-  } */
 
   @media (max-width: 500px) {
     font-size: 28px;
   }
 `
 
+const Logo = styled.img`
+  scale: 1.2;
+`
+
+const ContactContent = styled.div`
+  margin-left: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const ContactLink = styled.a`
+  color: inherit; // 부모 요소의 색상 상속
+  text-decoration: none; // 링크 밑줄 제거
+`
+
 const Introduction: FunctionComponent<IntroductionProps> = function ({
   profileImage,
 }) {
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        alert('클립보드에 복사되었습니다: ')
+      })
+      .catch(err => {
+        alert('복사에 실패했습니다: ' + err)
+      })
+  }
+
   return (
     <Background>
       <Wrapper>
@@ -126,9 +154,32 @@ const Introduction: FunctionComponent<IntroductionProps> = function ({
         <RowDiv>
           <ProfileImage profileImage={profileImage} />
           <Contact>
-            <ContactItem>Call : 010-5512-4339</ContactItem>
-            <ContactItem>Email : chltjdgh3@naver.com</ContactItem>
-            <ContactItem>Github : https://github.com/seonghoho</ContactItem>
+            <ContactItem>
+              <Logo src={Phone} alt="call" />
+              <ContactContent onClick={() => copyToClipboard('010-5512-4339')}>
+                010-5512-4339
+              </ContactContent>
+            </ContactItem>
+            <ContactItem>
+              <Logo src={Message} alt="email" />
+              <ContactContent
+                onClick={() => copyToClipboard('chltjdgh3@naver.com')}
+              >
+                chltjdgh3@naver.com
+              </ContactContent>
+            </ContactItem>
+            <ContactItem>
+              <Logo src={Github} alt="github_link" />
+              <ContactContent>
+                <ContactLink
+                  href="https://github.com/seonghoho"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  https://github.com/seonghoho
+                </ContactLink>
+              </ContactContent>
+            </ContactItem>
           </Contact>
         </RowDiv>
       </Wrapper>
--- src/components/Portfolio/Summary.tsx
+++ src/components/Portfolio/Summary.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef, useState } from 'react'
 import styled from '@emotion/styled'
 
 const Background = styled.div`
@@ -25,16 +26,78 @@ const Wrapper = styled.div`
   }
 `
 
-const Title = styled.div`
+const Title = styled.div<{ isVisible: boolean }>`
+  margin-top: 12vh;
   font-size: 40px;
   font-weight: 600;
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  transition: opacity 0.5s ease-in-out;
+
+  @media (max-width: 1050px) {
+    font-size: 30px;
+  }
+
+  @media (max-width: 730px) {
+    font-size: 20px;
+  }
+  @media (max-width: 500px) {
+    font-size: 16px;
+  }
 `
 
-const Summary = () => {
+const Summary: React.FC = () => {
+  const [isVisible, setIsVisible] = useState([false, false, false])
+  // useRef를 HTMLElement 배열로 타입 지정
+  const titleRefs = useRef<(HTMLElement | null)[]>([])
+
+  useEffect(() => {
+    const observers = titleRefs.current.map((ref, index) => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            setIsVisible(prev => {
+              const newIsVisible = [...prev]
+              newIsVisible[index] = true
+              return newIsVisible
+            })
+            observer.unobserve(entry.target)
+          }
+        },
+        {
+          root: null,
+          threshold: 1, // 화면의 40% 지점에 도달했을 때
+        },
+      )
+      return observer
+    })
+
+    titleRefs.current.forEach((ref, index) => {
+      if (ref) {
+        observers[index].observe(ref)
+      }
+    })
+
+    return () => {
+      titleRefs.current.forEach((ref, index) => {
+        if (ref) {
+          observers[index].unobserve(ref)
+        }
+      })
+    }
+  }, [])
+
   return (
     <Background>
       <Wrapper>
-        <Title>남들과 다른 성장곡선을 그리는 프론트엔드 개발자</Title>
+        <Title ref={el => (titleRefs.current[0] = el)} isVisible={isVisible[0]}>
+          남들과 다른 성장 곡선을 그리고 있습니다.
+        </Title>
+        <Title ref={el => (titleRefs.current[1] = el)} isVisible={isVisible[1]}>
+          대세를 따르기만 하는 것이 아닌, 이유에 집중합니다.
+        </Title>
+        <Title ref={el => (titleRefs.current[2] = el)} isVisible={isVisible[2]}>
+          팀 내에서 페이스 메이커 역할을 합니다.
+        </Title>
       </Wrapper>
     </Background>
   )
