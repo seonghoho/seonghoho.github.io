a267c275b1bd1eb02da8a74468443d7e8f91b55e
choiseongho <chltjdgh3@naver.com>
2024-07-14 22:08:24 +0900

    chore: NotoSansKR ttf 폰트 추가

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -2,3 +2,10 @@ declare module '*.svg' {
   const content: string
   export default content
 }
+
+// 폰트 파일을 선언하고 읽도록 만드는 코드
+
+declare module '*.ttf' {
+  const content: string
+  export default content
+}
