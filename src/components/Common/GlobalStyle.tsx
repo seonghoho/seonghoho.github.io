aa4c20a0c0498a0e00d077648229762e28cab011
choiseongho <chltjdgh3@naver.com>
2024-07-14 22:07:49 +0900

    feat: web font를 local font로 변경

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -1,8 +1,79 @@
 import React, { FunctionComponent } from 'react'
 import { css, Global } from '@emotion/react'
+// 폰트 파일 import
+import NotoSansKRThin from '../../fonts/NotoSansKR-Thin.ttf'
+import NotoSansKRExtraLight from '../../fonts/NotoSansKR-ExtraLight.ttf'
+import NotoSansKRLight from '../../fonts/NotoSansKR-Light.ttf'
+import NotoSansKRRegular from '../../fonts/NotoSansKR-Regular.ttf'
+import NotoSansKRMedium from '../../fonts/NotoSansKR-Medium.ttf'
+import NotoSansKRSemiBold from '../../fonts/NotoSansKR-SemiBold.ttf'
+import NotoSansKRBold from '../../fonts/NotoSansKR-Bold.ttf'
+import NotoSansKRExtraBold from '../../fonts/NotoSansKR-ExtraBold.ttf'
+import NotoSansKRBlack from '../../fonts/NotoSansKR-Black.ttf'
 
 const defaultStyle = css`
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap');
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRThin}) format('truetype');
+    font-weight: 100;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRExtraLight}) format('truetype');
+    font-weight: 200;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRLight}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRRegular}) format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRMedium}) format('truetype');
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRSemiBold}) format('truetype');
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRBold}) format('truetype');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRExtraBold}) format('truetype');
+    font-weight: 800;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${NotoSansKRBlack}) format('truetype');
+    font-weight: 900;
+    font-style: normal;
+  }
 
   * {
     font-family:
