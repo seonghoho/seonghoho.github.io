612cfb288f4145e96f6dba3407c8ad9b00679020
choiseongho <chltjdgh3@naver.com>
2024-10-16 18:05:39 +0900

    fix: 리디렉션 오류 수정

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -13,24 +13,24 @@ exports.onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
     <link
       rel="apple-touch-icon"
       sizes="76x76"
-      href="./static/apple-touch-icon.png"
+      href="/apple-touch-icon.png"
     />,
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href="./static/favicon-32x32.png"
+      href="/favicon-32x32.png"
     />,
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href="./static/favicon-16x16.png"
+      href="/favicon-16x16.png"
     />,
     // <link rel="manifest" href="./static/site.webmanifest" />,
     <link
       rel="mask-icon"
-      href="./static/safari-pinned-tab.svg"
+      href="/safari-pinned-tab.svg"
       color="#5bbad5"
     />,
     <meta name="msapplication-TileColor" content="#00aba9" />,
